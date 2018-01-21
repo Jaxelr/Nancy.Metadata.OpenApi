e8c24582d313bba3a95757c3e8e2fa10b7d86de1
Jaxel A Rojas <jaxelr@gmail.com>
2018-01-20 22:01:51 -0400

    Updated to the latest Swagger-UI Version. Removed incorrect API base definition.

--- Nancy.Metadata.OpenApi.DemoApplication/Modules/DocsModule.cs
+++ Nancy.Metadata.OpenApi.DemoApplication/Modules/DocsModule.cs
@@ -6,7 +6,7 @@ namespace Nancy.Metadata.OpenApi.DemoApplication.Modules
 {
     public class DocsModule : OpenApiDocsModuleBase
     {
-        public DocsModule(IRouteCacheProvider routeCacheProvider) : base(routeCacheProvider, "/api/docs", "Sample API documentation", "v1.0", "localhost:61963", "/api", "http")
+        public DocsModule(IRouteCacheProvider routeCacheProvider) : base(routeCacheProvider, "/api/docs", "Sample API documentation", "v1.0", "http://localhost:61963", "My Descripton", "/api")
         {
             Get("/", async (x, ct) => await Task.Run(() => Response.AsRedirect("/index.html")));
         }
