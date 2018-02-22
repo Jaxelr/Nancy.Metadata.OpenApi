1a1bfa5607c16b9a69dae20861cf40433ad305c2
Jaxel Rojas <jaxelr@gmail.com>
2018-02-22 16:36:15 -0400

    Added constructors for Multiple Server instances per specification. Marked as obsolete previous constructor for removal on future versions. Changed Demo Application to use newer constructor.

--- Nancy.Metadata.OpenApi.DemoApplication/Modules/DocsModule.cs
+++ Nancy.Metadata.OpenApi.DemoApplication/Modules/DocsModule.cs
@@ -1,4 +1,5 @@
-﻿using Nancy.Metadata.OpenApi.Modules;
+﻿using Nancy.Metadata.OpenApi.Model;
+using Nancy.Metadata.OpenApi.Modules;
 using Nancy.Routing;
 using System.Threading.Tasks;
 
@@ -6,7 +7,17 @@ namespace Nancy.Metadata.OpenApi.DemoApplication.Modules
 {
     public class DocsModule : OpenApiDocsModuleBase
     {
-        public DocsModule(IRouteCacheProvider routeCacheProvider) : base(routeCacheProvider, "/api/docs", "Sample API documentation", "v1.0", "http://localhost:61963", "My Descripton", "/api")
+        public DocsModule(IRouteCacheProvider routeCacheProvider) :
+            base(routeCacheProvider,
+                "/api/docs",
+                "Sample API documentation",
+                "v1.0",
+                new Server
+                {
+                    Url = "http://localhost:51637/",
+                    Description = "My Descripton"
+                },
+                "/api")
         {
             Get("/", async (x, ct) => await Task.Run(() => Response.AsRedirect("/index.html")));
         }
--- Nancy.Metadata.OpenApi/Modules/OpenApiDocsModuleBase.cs
+++ Nancy.Metadata.OpenApi/Modules/OpenApiDocsModuleBase.cs
@@ -10,31 +10,68 @@ namespace Nancy.Metadata.OpenApi.Modules
     public abstract class OpenApiDocsModuleBase : NancyModule
     {
         private const string CONTENT_TYPE = "application/json";
+        private const string DOCS_LOCATION = "/api/docs";
+        private const string API_VERSION = "1.0";
+        private const string TITLE = "My Title";
+        private const string HOST = "localhost:5000";
+        private const string HOST_DESCRIPTION = "My Localhost";
+        private const string API_BASE_URL = "/";
+        private Server defaultServer = new Server { Url = HOST, Description = HOST_DESCRIPTION };
 
         private OpenApiSpecification openApiSpecification;
         private readonly IRouteCacheProvider routeCacheProvider;
         private readonly string title;
         private readonly string apiVersion;
-        private readonly Server host;
+        private readonly Server[] hosts;
         private readonly string apiBaseUrl;
 
+        [System.Obsolete("Deprecated in favor of the usage of constructor with Server model.")]
         protected OpenApiDocsModuleBase(IRouteCacheProvider routeCacheProvider,
-            string docsLocation = "/api/docs",
-            string title = "API documentation",
-            string apiVersion = "1.0",
-            string host = "localhost:5000",
-            string hostDescription = "My Localhost",
-            string apiBaseUrl = "/")
+            string docsLocation = DOCS_LOCATION,
+            string title = TITLE,
+            string apiVersion = API_VERSION,
+            string host = HOST,
+            string hostDescription = HOST_DESCRIPTION,
+            string apiBaseUrl = API_BASE_URL) : this(
+                    routeCacheProvider,
+                    docsLocation,
+                    title,
+                    apiVersion,
+                    new Server[] { new Server { Url = host, Description = hostDescription } },
+                    apiBaseUrl)
+        {
+        }
+
+        protected OpenApiDocsModuleBase(IRouteCacheProvider routeCacheProvider,
+            string docsLocation,
+            string title,
+            string apiVersion,
+            Server host = null,
+            string apiBaseUrl = API_BASE_URL) : this(
+            routeCacheProvider,
+            docsLocation,
+            title,
+            apiVersion,
+            new Server[] { host },
+            apiBaseUrl)
+        {
+        }
+
+        protected OpenApiDocsModuleBase(IRouteCacheProvider routeCacheProvider,
+            string docsLocation,
+            string title,
+            string apiVersion,
+            Server[] hosts = null,
+            string apiBaseUrl = API_BASE_URL)
         {
             this.routeCacheProvider = routeCacheProvider;
             this.title = title;
             this.apiVersion = apiVersion;
-            this.host = new Server { Url = host, Description = hostDescription };
+            this.hosts = hosts;
             this.apiBaseUrl = apiBaseUrl;
 
             Get(docsLocation, r => GetDocumentation());
         }
-
 
         public virtual Response GetDocumentation()
         {
@@ -58,7 +95,7 @@ namespace Nancy.Metadata.OpenApi.Modules
                     Title = title,
                     Version = apiVersion,
                 },
-                Servers = new Server[] { host }
+                Servers = hosts
             };
 
             // generate documentation
