11f7f85f225566e7f905d39e54a513e4fee2f2ef
Jaxel Rojas <jaxelr@gmail.com>
2018-08-12 07:32:24 -0400

    Remove duplicate behavior test.

--- tests/UnitTests/MetadataTests.cs
+++ tests/UnitTests/MetadataTests.cs
@@ -10,22 +10,6 @@ namespace Nancy.Metadata.OpenApi.Tests.UnitTests
     public class MetadataTests
     {
         [Fact]
-        public void Generate_openapi_metadata()
-        {
-            //Arrange
-            var endpoint = new FakeEndpoint();
-            var fakeRouteDescription = new RouteDescription(endpoint.Operation, endpoint.Method, endpoint.Path, null);
-
-            //Act
-            var metadata = new OpenApiRouteMetadata(fakeRouteDescription);
-
-            //Assert
-            Assert.Equal(metadata.Path, fakeRouteDescription.Path);
-            Assert.Equal(metadata.Method, fakeRouteDescription.Method.ToLowerInvariant());
-            Assert.Equal(metadata.Name, fakeRouteDescription.Name);
-        }
-
-        [Fact]
         public void Generate_method_description()
         {
             //Arrange
