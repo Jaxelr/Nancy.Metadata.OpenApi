664ff0e8a7a83f9a01060c96b63eb975b06cafeb
Jaxel Rojas <jaxelr@gmail.com>
2019-01-04 07:22:14 -0400

    Bump to report generator to version 4.0.5

--- tools/testcoverage.bat
+++ tools/testcoverage.bat
@@ -9,7 +9,7 @@ mv coverage.xml ../%destination%/coverage.xml
 
 cd ../%destination%
 
-set "reportgenerator=%UserProfile%\.nuget\packages\reportgenerator\4.0.4\tools\net47\ReportGenerator.exe"
+set "reportgenerator=%UserProfile%\.nuget\packages\reportgenerator\4.0.5\tools\net47\ReportGenerator.exe"
 set "targetdir=."
 
 "%reportGenerator%" -reports:coverage.xml -reporttypes:HtmlInline -targetdir:%targetdir%
