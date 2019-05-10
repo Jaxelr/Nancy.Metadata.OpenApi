97286c4e899452e7c8cd213942633879af0488c1
Jaxel Rojas <jaxelr@gmail.com>
2019-05-09 20:41:30 -0400

    fix: adding the equatable interface for comparison ops

--- src/Model/Security.cs
+++ src/Model/Security.cs
@@ -1,9 +1,13 @@
-﻿namespace Nancy.Metadata.OpenApi.Model
+﻿using System;
+
+namespace Nancy.Metadata.OpenApi.Model
 {
-    public class Security
+    public class Security : IEquatable<Security>
     {
         public string Key { get; set; }
 
         public string[] Scopes { get; set; }
+
+        public bool Equals(Security other) => (Key == other.Key);
     }
 }
