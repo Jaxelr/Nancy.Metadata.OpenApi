f6dbd09896f5fa49f478762d2b19b96d819c479c
Jaxel Rojas <jaxelr@gmail.com>
2019-05-24 07:25:36 -0400

    test: added unit test for equality on Security model

---
+++ tests/UnitTests/ModelTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace Nancy.Metadata.OpenApi.Tests.UnitTests
+{
+    public class ModelTests
+    {
+        [Theory]
+        [InlineData("key1")]
+        public void Security_equality_true(string key)
+        {
+            //Arrange
+            var security1 = new Model.Security() { Key = key };
+            var security2 = new Model.Security() { Key = key };
+
+            //Act
+            bool result = security1.Equals(security2);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("key1", "key2")]
+        public void Security_equality_false(string key1, string key2)
+        {
+            //Arrange
+            var security1 = new Model.Security() { Key = key1 };
+            var security2 = new Model.Security() { Key = key2 };
+
+            //Act
+            bool result = security1.Equals(security2);
+
+            //Assert
+            Assert.False(result);
+        }
+    }
+}
