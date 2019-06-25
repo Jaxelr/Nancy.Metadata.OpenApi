f8b32d611dae38fb59d024bcba3d76bf9faffc4e
Jaxel Rojas <jaxelr@gmail.com>
2019-06-25 06:27:58 -0400

    docs: added comment to Security model

--- src/Model/Security.cs
+++ src/Model/Security.cs
@@ -2,6 +2,10 @@
 
 namespace Nancy.Metadata.OpenApi.Model
 {
+    /// <summary>
+    /// No need to decorate this class for Json Props since this
+    /// is calculated on the fly by the custom collection json converter
+    /// </summary>
     public class Security : IEquatable<Security>
     {
         public string Key { get; set; }
