a58def2b1ec7669392fb6d57c2917709ea99ffec
Jaxel Rojas <jaxelr@gmail.com>
2019-07-10 05:26:39 -0400

    tests: added FakeServer
    This is an expanded server definition with variables

---
+++ tests/Fakes/FakeServer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Nancy.Metadata.OpenApi.Model;
+
+namespace Nancy.Metadata.OpenApi.Tests.Fakes
+{
+    public class FakeServer
+    {
+        private static readonly string[] Enums = { "8080", "8088" };
+        internal const string FakeKey = "port";
+        internal static readonly ServerVariable ServerVariable = new ServerVariable() { Enum = Enums, Default = "demo", Description = "This is a sample description" };
+        private static readonly Dictionary<string, ServerVariable> InternalDict = new Dictionary<string, ServerVariable>() { { FakeKey, ServerVariable } };
+
+        public static Server Server = new Server() { Url = "http://localhost:{port}/", Description = "Sample expanded server", Variables = InternalDict };
+    }
+}
