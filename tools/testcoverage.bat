32577fbdde3f13886e8819ab978b34bed476fd33
Jaxel Rojas <jaxelr@gmail.com>
2019-08-01 06:39:16 -0400

    chore: bump to report generator dep to 4.2.12

--- tools/testcoverage.bat
+++ tools/testcoverage.bat
@@ -9,7 +9,7 @@ mv coverage.xml ../%destination%/coverage.xml
 
 cd ../%destination%
 
-set "reportgenerator=%UserProfile%\.nuget\packages\reportgenerator\4.2.10\tools\net47\ReportGenerator.exe"
+set "reportgenerator=%UserProfile%\.nuget\packages\reportgenerator\4.2.12\tools\net47\ReportGenerator.exe"
 set "targetdir=."
 
 "%reportGenerator%" -reports:coverage.xml -reporttypes:HtmlInline -targetdir:%targetdir%
